62b08eca57bb8cefa0833375f9d211e2fd441fa2
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-07-06 14:50:48 -0400

    fixes broken image

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {css, StyleSheet} from 'aphrodite';
+import React, {Component} from 'react'
+import {css, StyleSheet} from 'aphrodite'
 
 const styles = StyleSheet.create({
   root: {
@@ -41,44 +41,43 @@ const styles = StyleSheet.create({
     fontFamily: "'Inconsolata', monospace",
     fontSize: '0.95em'
   }
-});
+})
 
 export default class Home extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       loading: true,
     }
   }
 
-  componentWillMount() {
-    this.getProfileImage()
+  componentDidMount() {
+    this.getProfile()
       .then(data => this.setState({loading: false, ...data}))
   }
 
-  getProfileImage = async () =>
+  getProfile = async () =>
     await fetch('https://api.github.com/users/JFrankfurt')
-      .then(res => res.json());
+      .then(res => res.json())
 
   render() {
     return (
       <div className={css(styles.root)}>
         <div className={css(styles.profile)}>
           <div className={css(styles.imageContainer)}>
-            <img className={css(styles.image)}
-                 src="https://pbs.twimg.com/profile_images/913500473029746688/gGY-3tGY_400x400.jpg"/>
+            <img className={css(styles.image)} src={this.state.avatar_url}/>
           </div>
           <div className={css(styles.summary)}>
             <div className={css(styles.title)}>
-              <h2>Jordan Frankfurt</h2><span>&nbsp;(<a href="https://www.twitter.com/JordanFrankfurt">twitter</a> | <a
-              href="https://earn.com/jordanfrankfurt/">contact me</a>)</span>
+              <h2>Jordan Frankfurt</h2><span>&nbsp;(<a href="https://www.twitter.com/JordanFrankfurt">twitter</a>
+              | <a href="https://keybase.io/jordanfrankfurt">contact me</a>)</span>
             </div>
             <p>Software Engineer{`\u2013`}Brooklyn, NY.</p>
+            <pre className={css(styles.monoSpaced)}> day job: eth dapps @ Consensys</pre>
             <br/>
             <b>Topics of interest:</b>
             <pre className={css(styles.monoSpaced)}> structured credit (nonagy cmos and servicing)</pre>
-            <pre className={css(styles.monoSpaced)}> cryptography (range proofs, Cuckoo Cycle)</pre>
-            <pre className={css(styles.monoSpaced)}> distributed systems</pre>
+            <pre className={css(styles.monoSpaced)}> cryptography (Mimblewimble! ❤️)</pre>
             <pre className={css(styles.monoSpaced)}> reinforcement learning w/ implicit feedback</pre>
             <pre className={css(styles.monoSpaced)}> history of monetary systems</pre>
             <br/>
@@ -87,20 +86,17 @@ export default class Home extends Component {
             <pre className={css(styles.monoSpaced)}> sql, nosql, and in memory data stores</pre>
             <pre className={css(styles.monoSpaced)}> microservice arch. (messaging, service discovery)</pre>
             <br/>
-            {
-              !this.state.loading
-                ? <div>
-                  <h3><a href={this.state.html_url}>Github:</a></h3>
-                  <pre className={css(styles.monoSpaced)}>followers: {this.state.followers}</pre>
-                  <pre className={css(styles.monoSpaced)}>following: {this.state.following}</pre>
-                  <pre className={css(styles.monoSpaced)}>open source repos: {this.state.public_repos}</pre>
-                  <pre className={css(styles.monoSpaced)}>last updated: {this.state.updated_at}</pre>
-                </div>
-                : null
-            }
+            {!this.state.loading
+            && <div>
+              <h3><a href={this.state.html_url}>Github:</a></h3>
+              <pre className={css(styles.monoSpaced)}>followers: {this.state.followers}</pre>
+              <pre className={css(styles.monoSpaced)}>following: {this.state.following}</pre>
+              <pre className={css(styles.monoSpaced)}>open source repos: {this.state.public_repos}</pre>
+              <pre className={css(styles.monoSpaced)}>last updated: {this.state.updated_at}</pre>
+            </div>}
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
